98a13c98f815bda3c756b84754378f0b25d1e7cd
Petro Ratych <128933823+AskAfter@users.noreply.github.com>
2025-06-16 11:50:37 +0300

    Update UI design

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,18 +4,19 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        secondary: "bg-accent text-accent-foreground hover:bg-accent/80",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-10 px-5",
+        sm: "h-9 rounded-lg px-4",
+        lg: "h-11 rounded-lg px-8",
       },
     },
     defaultVariants: {
